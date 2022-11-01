36e20ee291191277b68eda7211699cda810e94c1
sweetisalunkhe <sweetikhot6618@gmail.com>
2022-11-01 20:22:16 +0530

    added code for add and sub

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    c = a+b
+    return c
+
+
+def sub(a,b):
+    c=a-b
+    return c
+
+
+a= 10
+b=5
+print(f" addition of {a} and {b} is {add(a,b)}")
+
+print(f" subtraction of {a} and {b} is {sub(a,b)}")
